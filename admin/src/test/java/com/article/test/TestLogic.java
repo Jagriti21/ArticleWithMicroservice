d39f6cdf6751792c3b793da536f6aa800f02b4da
jagriti <jagriti.s21@gmail.com>
2019-09-18 12:12:20 +0530

    Junit

--- admin/src/test/java/com/article/test/TestLogic.java
+++ admin/src/test/java/com/article/test/TestLogic.java
@@ -2,25 +2,39 @@ package com.article.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.article.admin.entity.Category;
-import com.article.admin.service.ICategoryService;
+import com.article.admin.repo.CategoryDAO;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = com.article.admin.UserApplication.class)
 public class TestLogic {
 	@Autowired
-	private ICategoryService i;
-	  @Test  
-	    public void testFindCategory(){  
-		  Category cat = new Category();
-		  cat=i.getCategoryById(11);
-		  String yes = cat.getCategoryname();
-		  System.out.println(yes);
-		  String category="Horror";
-		  assertEquals(category,category);
-		  //  i assertNotNull(i.getCategoryById(categoryId));
-	      //  assertEquals(4,i.getCategoryById(categoryId));  
-	       // assertEquals(-1,Calculation.findMax(new int[]{-12,-1,-3,-4,-2}));  
-	    }  
+	private CategoryDAO i;
+
+
+@Test
+   public void myRepo(){
+      List<Category> list = new ArrayList<>();
+      list = i.getAllCategory();
+      assertEquals(list.get(0).getCategoryname(), "Adventure");
+   }
+
+
+@Test
+public void myRepo1(){
+   List<Category> list = new ArrayList<>();
+   list = i.getAllCategory();
+   assertEquals(list.get(1).getCategoryname(), "novel");
+}
+
+
 }
